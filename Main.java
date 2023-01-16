8c18c515c1504c13ec1cf7211f3f8e9f0ac567af
Goodness <paul_assiaw@hotmail.co.uk>
2023-01-16 15:19:10 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,11 @@
+class Main {
+  public static void main(String[] args) {
+    StringBuilder sb = new StringBuilder(); 
+    sb.append("Hello"); 
+    sb.append(" to"); 
+    sb.append(" you"); 
+    System.out.println(sb.toString());
+
+
+  }
+}
